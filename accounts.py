e76488beb50fa431a43cec411896db09a775aaf4
Evan Young <eyoung@winnipeg.ca>
2019-12-20 12:52:59 -0600

    Update to session 130 Methods Part 1 at 5:27

--- accounts.py
+++ accounts.py
@@ -1,15 +1,20 @@
+import datetime
+import pytz
+
 class Account:
     """ Simple account class with ballance """
 
     def __init__(self, name, balance):
         self.name = name
         self.balance = balance
+        self.transaction_list = []
         print(f"Account created for {self.name}")
 
     def deposit(self, amount):
         if amount > 0:
             self.balance += amount
             self.show_balance()
+            self.transaction_list.append((pytz.utc.localize(datetime.datetime.utcnow()), amount))
     
     def withdraw(self, amount):
         if 0 < amount <= self.balance:
@@ -20,6 +25,15 @@ class Account:
 
     def show_balance(self):
         print(f"Balance is {self.balance}")
+
+    def show_transactions(self):
+        for date, amount in self.transaction_list:
+            if amount > 0:
+                tran_type = "deposited"
+            else:
+                tran_type = "withdrawn"
+                amount *= -1
+            print(f"{amount:6} {tran_type} on {date} (local time was {date.astimezone()})")
 
 
 if __name__ == "__main__":
