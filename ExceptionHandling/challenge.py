1752e8c78c5748f831a8f57c8944f592fac5d4de
Evan Young <eyoung@winnipeg.ca>
2020-01-31 16:51:12 -0600

    end of day Friday

--- ExceptionHandling/challenge.py
+++ ExceptionHandling/challenge.py
@@ -4,9 +4,9 @@ SECOND = input("Second Number: ")
 
 try:
     RESULT = int(FIRST) // int(SECOND)
-    print(f"Result: {RESULT}")
+    print(f"Result of {FIRST} devided by {SECOND} is {RESULT}")
 except ValueError:
-    #handle convert from string to int error
+    #handle convert from string to int error TODO Remove this
     print("Please enter an integer.")
 except ZeroDivisionError:
     print("You can't devide by zero")
---
+++ RollingBack/rollback.py
@@ -0,0 +1,92 @@
+""" Account Class """
+import sqlite3
+
+DB = sqlite3.connect("RollingBack\\account.sqlite")
+DB.execute("CREATE TABLE IF NOT EXISTS accounts (name TEXT PRIMARY KEY NOT NULL, \
+    balance INTEGER NOT NULL)")
+DB.execute("CREATE TABLE IF NOT EXISTS transactions (time TIMESTAMP NOT NULL, \
+    account TEXT NOT NULL, amount INTEGER NOT NULL, PRIMARY KEY (time, account))")
+
+class Account():
+    """ Class to represent an Account """
+
+    def __init__(self, name: str, opening_balance: int = 0):
+        """ Initialize
+
+        Arguments:
+            name {str} -- Name of account holder
+
+        Keyword Arguments:
+            opening_balance {int} -- Opening Balance (default: {0.0})
+        """
+
+        cursor = DB.execute("SELECT name, balance FROM accounts WHERE (name = ?)", (name,))
+        row = cursor.fetchone()
+
+        if row:
+            self.name, self._balance = row
+            print(f"Retrieved record for {self.name}", end='')
+        else:
+            self.name = name
+            self._balance = opening_balance
+            cursor.execute("INSERT INTO accounts VALUES(?, ?)", (name, opening_balance))
+            cursor.connection.commit()
+            print(f"Account created for {self.name}", end='')
+        self.show_balance()
+
+    def deposit(self, amount: int) -> float:
+        """ Deposit into account
+
+        Arguments:
+            amount {float} -- Amount to deposit
+
+        Returns:
+            float -- Current Balance
+        """
+        if amount > 0.0:
+            self._balance += amount
+            print(f"{amount/100:.2f} deposited")
+        return self._balance/100
+
+    def withdraw(self, amount: int) -> float:
+        """ Withdraw from account
+
+        Arguments:
+            amount {float} -- Amount to withdraw
+
+        Returns:
+            float -- Current Balance
+        """
+        if 0 < amount <= self._balance:
+            self._balance -= amount
+            print(f"{amount/100:.2f} withdrawn")
+            return amount /100
+        else:
+            print(
+                "The amount must be greater than zero and no more than your account balance")
+            return 0.0
+
+    def show_balance(self):
+        """ Display Account Balance
+
+        Returns:
+            string -- Current Balance
+        """
+        print(f"Balance on account {self.name} is {self._balance/100:.2f}")
+
+
+if __name__ == "__main__":
+    JOHN = Account("John")
+    JOHN.deposit(1010)
+    JOHN.deposit(10)
+    JOHN.deposit(10)
+    JOHN.withdraw(30)
+    JOHN.withdraw(0)
+    JOHN.show_balance()
+
+    TERRY = Account("Terry")
+    GRAHAM = Account("Graham", 9000)
+    ERIC = Account("ERIC", 7000)
+
+    DB.close()
+    
